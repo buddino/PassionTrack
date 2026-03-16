cdf8b16d0ee6c5e7e10ef342ef0d73e7b159f24b
budda <cuffaro.giovanni@gmail.com>
2026-03-16 17:14:46 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const withPWA = withPWAInit({
 });
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true
+    }
+};
 
 export default withPWA(nextConfig);
